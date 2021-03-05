13008d27f613d8271afeebe9e0bbb71142e553ef
Kristen-Li <62110657+Kristen-Li@users.noreply.github.com>
2021-03-04 19:46:53 -0500

    Delete Distance Vector directory

--- Distance Vector/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z $1 ]; then
-    echo "Usage: run.sh <topology>"
-    echo "(do not include the file extension, e.g., 'run.sh foo' to use foo.txt)"
-else
-    python run_topo.py $1.txt $1.log
-    python output_validator.py $1.log
-fi
