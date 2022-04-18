a25bb7e2dd34294f125fd8552f1e8e87f0a29206
Kristen-Li <62110657+Kristen-Li@users.noreply.github.com>
2022-04-18 15:20:24 -0700

    Update experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -1,4 +1,6 @@
-"""experiment2
+"""
+ experiment2
+ this code will experiment of different impact and see the effect on trading behavior for the in-sample data
 """
 
 import pandas as pd
@@ -8,14 +10,12 @@ import marketsimcode
 import StrategyLearner as sl
 
 def author():
-    return('wli626')
+    return 'wli626'
     
 if __name__ == "__main__":  
     sym = 'JPM'
     sd = dt.datetime(2008,1,1)
     ed = dt.datetime(2009,12,31)
-#     sd = dt.datetime(2010,1,1)
-#     ed = dt.datetime(2011,12,31)
     sv = 100000
     
     learner  = sl.StrategyLearner(verbose=False, impact=0.0005)
@@ -45,18 +45,17 @@ if __name__ == "__main__":
     values  /= sv
     values2 /= sv
     values3 /= sv
-    values4 /= sv
-    values5 /= sv
+
     
-    fig = plt.figure(figsize=(10,5), dpi=80)
+    fig = plt.figure(figsize=(12,6), dpi=80)
     plt.plot(values,  label='impact: 0.00')
     plt.plot(values2, label='impact: 0.0005')
     plt.plot(values3, label='impact: 0.005')
    
     
-    plt.xlabel('Dates', fontsize=14)
-    plt.ylabel('Portfolio value', fontsize=14)
-    fig.suptitle('Impact value comparison', fontsize=18)
+    plt.xlabel('Dates', fontsize=13)
+    plt.ylabel('Portfolio value', fontsize=13)
+    fig.suptitle('Porfolio Values for different Impact Values', fontsize=17)
     fig.legend(loc=3, bbox_to_anchor=(0.08, 0.6))
     plt.show()
     
@@ -75,7 +74,7 @@ if __name__ == "__main__":
    
 
     # Compare portfolio against benchmark
-    print("=== Machine Learning Strategy (MLS) V.S. Manual Strategy (MS) In Sample ===")
+    print("=== Strategy  V.S. Manual Strategy  In Sample ===")
     print("Date Range: {} to {}".format(sd, ed))
     print()
     print("Sharpe Ratio of 1: {}".format(port_sr_1))
@@ -86,13 +85,13 @@ if __name__ == "__main__":
     print("Cumulative Return of 2: {}".format(port_cr_2))
     print("Cumulative Return of 3 : {}".format(port_cr_3))
     print()
-    print("Standard Deviation of 1: {}".format(port_stddr_1))
-    print("Standard Deviation of 2: {}".format(port_stddr_2))
-    print("Standard Deviation of 3 : {}".format(port_stddr_3))
-    print()
     print("Average Daily Return of 1: {}".format(port_adr_1))
     print("Average Daily Return of 2: {}".format(port_adr_2))
     print("Average Daily Return of 3 : {}".format(port_adr_3))
+    print()
+    print("Standard Deviation of 1: {}".format(port_stddr_1))
+    print("Standard Deviation of 2: {}".format(port_stddr_2))
+    print("Standard Deviation of 3 : {}".format(port_stddr_3))
     print()
     print("Final Portfolio Value 1: {}".format(values[-1]))
     print("Final  Portfolio Value 2: {}".format(values2[-1]))
