ac9087a9abdc2bbf41b3b5fbed302ed66c90a74c
Kristen-Li <62110657+Kristen-Li@users.noreply.github.com>
2022-04-18 13:36:51 -0700

    Update experiment2.py

--- experiment2.py
+++ experiment2.py
@@ -1,7 +1,4 @@
 """experiment2
-Python 3.6
-CS7646 Project 8 - Strategy Learner
-Mike Tong (mtong31)
 """
 
 import pandas as pd
@@ -10,48 +7,39 @@ import matplotlib.pyplot as plt
 import marketsimcode
 import StrategyLearner as sl
 
-"""This code compares the performance of the strategy learner under various impact values as well as the 
-respective benchmark (buy and hold 1000 shares).
-
-The code will print a plot of the normalized gain, as well as a dataframe with the respective values"""
 def author():
     return('wli626')
     
-if __name__ == "__main__":
-    print('Project 8 Experiment2: mtong31')
+if __name__ == "__main__":  
     sym = 'JPM'
     sd = dt.datetime(2008,1,1)
     ed = dt.datetime(2009,12,31)
-    sd2 = dt.datetime(2010,1,1)
-    ed2 = dt.datetime(2011,12,31)
+#     sd = dt.datetime(2010,1,1)
+#     ed = dt.datetime(2011,12,31)
     sv = 100000
     
-    learner  = sl.StrategyLearner(verbose=False, impact=0.00)
-    learner2 = sl.StrategyLearner(verbose=False, impact=0.0005)
-    learner3 = sl.StrategyLearner(verbose=False, impact=0.002)
-    learner4 = sl.StrategyLearner(verbose=False, impact=0.005)
-    learner5 = sl.StrategyLearner(verbose=False, impact=1.00)
+    learner  = sl.StrategyLearner(verbose=False, impact=0.0005)
+    learner2 = sl.StrategyLearner(verbose=False, impact=0.005)
+    learner3 = sl.StrategyLearner(verbose=False, impact=0.05)
+
 
     # Training phase
     learner.addEvidence(sym, sd, ed, 100000)
     learner2.addEvidence(sym, sd, ed, 100000)
     learner3.addEvidence(sym, sd, ed, 100000)
-    learner4.addEvidence(sym, sd, ed, 100000)
-    learner5.addEvidence(sym, sd, ed, 100000)
+   
     
     # Test Phase
     strategy  = learner.testPolicy(sym, sd, ed)
     strategy2 = learner2.testPolicy(sym, sd, ed)
     strategy3 = learner3.testPolicy(sym, sd, ed)
-    strategy4 = learner4.testPolicy(sym, sd, ed)
-    strategy5 = learner5.testPolicy(sym, sd, ed)
+   
     
     # Performance Metrics
     values  = marketsimcode.compute_portvals(strategy)
     values2 = marketsimcode.compute_portvals(strategy2)
     values3 = marketsimcode.compute_portvals(strategy3)
-    values4 = marketsimcode.compute_portvals(strategy4)
-    values5 = marketsimcode.compute_portvals(strategy5)
+
 
     # Normalize gains to the sv
     values  /= sv
@@ -63,9 +51,8 @@ if __name__ == "__main__":
     fig = plt.figure(figsize=(10,5), dpi=80)
     plt.plot(values,  label='impact: 0.00')
     plt.plot(values2, label='impact: 0.0005')
-    plt.plot(values3, label='impact: 0.002')
-    plt.plot(values4, label='impact: 0.005')
-    plt.plot(values5, label='impact: 1.00')
+    plt.plot(values3, label='impact: 0.005')
+   
     
     plt.xlabel('Dates', fontsize=14)
     plt.ylabel('Portfolio value', fontsize=14)
@@ -75,11 +62,39 @@ if __name__ == "__main__":
     
     columns = len(learner.xTrain.columns)
     result_df = pd.concat([values, values2, values3, values4, values5],axis=1)
-    result_df.rename(columns={0 : 'impact: 0.00',\
-                              1 : 'impact: 0.0005',\
-                              2 : 'impact: 0.002',\
-                              3 : 'impact: 0.005',\
-                              4 : 'impact: 1.00'}, inplace=True)
+    result_df.rename(columns={0 : 'impact: 0.0005',\
+                              1 : 'impact: 0.005',\
+                              2 : 'impact: 0.05'
+                           }, inplace=True)
     
     print('Performance comparison: ')
     print(result_df)
+    port_cr_1, port_adr_1, port_stddr_1, port_sr_1 =marketsimcode.get_portfolio_stats(values)
+    port_cr_2, port_adr_2, port_stddr_2, port_sr_2 =marketsimcode.get_portfolio_stats(values2)
+    port_cr_3, port_adr_3, port_stddr_3, port_sr_3 =marketsimcode.get_portfolio_stats(values3)
+   
+
+    # Compare portfolio against benchmark
+    print("=== Machine Learning Strategy (MLS) V.S. Manual Strategy (MS) In Sample ===")
+    print("Date Range: {} to {}".format(sd, ed))
+    print()
+    print("Sharpe Ratio of 1: {}".format(port_sr_1))
+    print("Sharpe Ratio of 2: {}".format(port_sr_2))
+    print("Sharpe Ratio of 3: {}".format(port_sr_3))
+    print()
+    print("Cumulative Return of 1: {}".format(port_cr_1))
+    print("Cumulative Return of 2: {}".format(port_cr_2))
+    print("Cumulative Return of 3 : {}".format(port_cr_3))
+    print()
+    print("Standard Deviation of 1: {}".format(port_stddr_1))
+    print("Standard Deviation of 2: {}".format(port_stddr_2))
+    print("Standard Deviation of 3 : {}".format(port_stddr_3))
+    print()
+    print("Average Daily Return of 1: {}".format(port_adr_1))
+    print("Average Daily Return of 2: {}".format(port_adr_2))
+    print("Average Daily Return of 3 : {}".format(port_adr_3))
+    print()
+    print("Final Portfolio Value 1: {}".format(values[-1]))
+    print("Final  Portfolio Value 2: {}".format(values2[-1]))
+    print("Final Portfolio Value 3: {}".format(values3[-1]))
+    print()
